7bfa947570d1f6c498453c27915b01802c60f2ab
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2023-10-17 16:37:28 +0530

    chapter 17 - macros

--- chapter17/src/main.rs
+++ chapter17/src/main.rs
@@ -139,20 +139,100 @@ macro_rules! assert_equal_len {
 }
 
 macro_rules! op {
-    ($func:ident, $bound:ident, $op:tt,) => {
-        fn $func<T :$bound<T, Output=T> + Copy>(xs: &mut Vec<T>, ys: &Vec<T>) {
+    ($func:ident, $bound:ident, $op:tt, $method:ident) => {
+        fn $func<T: $bound<T, Output = T> + Copy>(xs: &mut Vec<T>, ys: &Vec<T>) {
             assert_equal_len!(xs, ys, $func, $op);
 
             for (x, y) in xs.iter_mut().zip(ys.iter()) {
-
+                *x = $bound::$method(*x, *y);
+                // *x = x.$method(*y)
             }
         }
     };
+}
+
+// implement `add_assign`, `mul_assign` and `sub_assign`
+op!(add_assign, Add, +=, add);
+op!(mul_assign, Mul, *=, mul);
+op!(sub_assign, Sub, -=, sub);
+
+mod test {
+    use std::iter;
+    macro_rules! test {
+        ($func:ident, $x:expr, $y:expr, $z:expr) => {
+            #[test]
+            fn $func() {
+                for size in 0_usize..10 {
+                    let mut x: Vec<_> = iter::repeat($x).take(size).collect();
+                    let y: Vec<_> = iter::repeat($y).take(size).collect();
+                    let z: Vec<_> = iter::repeat($z).take(size).collect();
+
+                    super::$func(&mut x, &y);
+                    assert_eq!(x, z);
+                }
+            }
+        };
+    }
+
+    // test
+    test!(add_assign, 1u32, 2u32, 3u32);
+    test!(mul_assign, 2u32, 3u32, 6u32);
+    test!(sub_assign, 3u32, 2u32, 1u32);
+}
+
+/* Domain Specific Language */
+// it is a mini language embedded in rust
+// this allows for concise syntax for special functionality
+
+// e.g. we want a calculator api to be built which works as
+// eval 1+2 -> 1 + 2 = 3
+
+macro_rules! calculate {
+    (eval $e:expr) => {
+        let val:usize = $e;
+        println!("{} = {}", stringify!{$e}, val);
+    };
+}
+
+fn part5() {
+    calculate! {
+        eval 1 + 2
+    }
+    calculate! {
+        eval (1+2)* (3/4)
+    }
+}
+
+/* Variadic Interface */
+// to allow for a variable number of arguments
+macro_rules! calculate2 {
+    (eval $e:expr) => {
+        let val:usize = $e;
+        println!("{} = {}", stringify!($e), val);
+    };
+
+    // decompose eval's recursively
+    (eval $e:expr, $(eval $es:expr), +) => {
+        {
+            calculate2! {eval $e}
+            calculate2! { $(eval $es), +}
+        }
+    };
+}
+
+fn part6() {
+    calculate2! {
+        eval 1+2,
+        eval 3+4,
+        eval (2*3)+1
+    }
 }
 
 fn main() {
     // part1();
     // part2();
     // part3();
-    part4();
+    // part4();
+    // part5();
+    part6();
 }
